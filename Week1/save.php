dad2aadb034e12c57b76f4049d8b4cfe2724b7cc
Ilari Juvani <ilari.juvani@metropolia.fi>
2015-03-28 15:12:51 +0200

    uusia filuja

---
+++ Week1/save.php
@@ -0,0 +1,40 @@
+<?php
+    $host = 'mysql.metropolia.fi';
+    $dbname = ''; // your username
+    $user = ''; // your username
+    $pass = ''; // your database password
+	
+   // TODO: get the data from the form by using $_POST
+   if( !empty($_POST['name']) or !empty($_POST['desc']) or !empty($_POST['date'])):
+	$data = array('name' => $_POST['name'], 'description' => $_POST['desc'], 'email' => $_POST['email'], 'phonenum' => $_POST['cell']);
+	$date = $_POST['date'];
+	$time = $_POST['time'];
+   // this is how you convert the date from the form to SQL formatted date:
+   $data['sqldate'] = date ("Y-m-d H:i:s", strtotime($date.' '.$time));
+   
+// this part was in dbConnect.php in last period:
+try {
+
+	$DBH = new PDO("mysql:host=$host;dbname=$dbname", $user, $pass);
+	$DBH->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+	$DBH->query("SET NAMES 'UTF8';");
+
+}catch(PDOException $e) {
+
+	echo "Could not connect to database.";
+	file_put_contents('log.txt', $e->getMessage(), FILE_APPEND);
+}
+
+    
+try {
+	// TODO: insert the data from the form to database table 'calendar'
+	
+$STH = $DBH->prepare("INSERT INTO calendar (eName, eDescription, pEmail, pPhone, eDate) values (:name, :description, :email, :phonenum, :sqldate)");
+$STH->execute($data);
+	
+} catch (PDOException $e) {
+	echo 'Something went wrong';
+	file_put_contents('log.txt', $e->getMessage()."\n\r", FILE_APPEND); // remember to set the permissions so that log.txt can be created
+}
+endif;
+?>
